eb5afa0994b0cd7cdf81ab8344ed386a64e3211e
Tran Anh Minh <trananhminh910@gmail.com>
2020-10-03 19:15:02 +0700

    first commit

---
+++ app/src/main/java/com/example/intentandroid/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.intentandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class LoginActivity {
+    private Button btnLogin;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/intentandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
